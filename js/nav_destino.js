2d88b9ca435bc5b8604284a9c7ae4b2bac7f921a
Joso2410 <blackmagicoficial53@gmail.com>
2025-08-06 10:49:30 -0400

    Update nav_destino.js

--- js/nav_destino.js
+++ js/nav_destino.js
@@ -56,3 +56,73 @@
 
 
 
+
+
+
+
+
+    
+
+
+
+
+
+
+
+/*Destino_inicio2*/
+
+
+    document.getElementById("Inicio2").addEventListener("click", function() {
+      // Scroll suave hacia el destino
+      document.getElementById("Inicio_Destino").scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+
+
+/*Destino_inicio2*/
+
+
+    document.getElementById("Acerca_de2").addEventListener("click", function() {
+      // Scroll suave hacia el destino
+      document.getElementById("Acerca_de_Destino").scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+
+
+/*Destino_inicio2*/
+
+
+    document.getElementById("Servicios2").addEventListener("click", function() {
+      // Scroll suave hacia el destino
+      document.getElementById("Servicios_Destino").scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+
+
+/*Destino_inicio2*/
+
+
+    document.getElementById("Portafolio2").addEventListener("click", function() {
+      // Scroll suave hacia el destino
+      document.getElementById("Portafolio_Destino").scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+
+
+
+/*Destino_inicio2*/
+
+
+    document.getElementById("Contacto2").addEventListener("click", function() {
+      // Scroll suave hacia el destino
+      document.getElementById("Contacto_Destino").scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+
+
+    
